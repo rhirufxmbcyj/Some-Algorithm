d01b9ad730aab944e872c19002811ccbd0c8ab26
liu_wan001 <liu_wan001@163.com>
2019-01-12 22:57:10 +0800

    修改目录

--- AStarSearch/src/AStar.cpp
+++ AStarSearch/src/AStar.cpp
@@ -4,65 +4,65 @@
 using std::sort;
 
 vector<a_node*> open_list;
-
-void set_node_variable(a_node *curr_node,a_node *next_node,a_node *_end)
-{
-    next_node->h = (abs(next_node->x - _end->x) + abs(next_node->y - _end->y))*STEP_LENGTH;
-    next_node->g = curr_node->g + STEP_LENGTH;
-    next_node->parent = curr_node;
-}
-
 int insert_neighbor_to_openlist(a_node *curr_node, a_node *_end, a_node _map[MAP_ROW][MAP_COLUMN])
 {
-    a_node *next_node = NULL;
+    a_node *tmp = NULL;
     vector<a_node*>::iterator is_contain;
     //取到当前节点的上下左右节点加入open列表 并设置这些节点的parent为当前节点
     //坐标系与数组下标有所不同
     //上
     if (curr_node->x > 0)
     {
-        next_node = &_map[curr_node->x-1][curr_node->y];
-        is_contain = std::find(open_list.begin(), open_list.end(), next_node);
-        if (next_node->attr != ATTR_FORBID && next_node->attr != ATTR_CLOSE && is_contain == open_list.end())
+        tmp = &_map[curr_node->x-1][curr_node->y];
+        is_contain = std::find(open_list.begin(), open_list.end(), tmp);
+        if (tmp->attr != ATTR_FORBID && tmp->attr != ATTR_CLOSE && is_contain == open_list.end())
         {
-            set_node_variable(curr_node, next_node, _end);
-            open_list.push_back(next_node);
+            tmp->h = (abs(tmp->x - _end->x) + abs(tmp->y - _end->y))*STEP_LENGTH;
+            tmp->g = curr_node->g + STEP_LENGTH;
+            tmp->parent = curr_node;
+            open_list.push_back(tmp);
         }
     }
 
     //下
     if (curr_node->x < MAP_ROW - 1)
     {
-        next_node = &_map[curr_node->x+1][curr_node->y];
-        is_contain = std::find(open_list.begin(), open_list.end(), next_node);
-        if (next_node->attr != ATTR_FORBID && next_node->attr != ATTR_CLOSE && is_contain == open_list.end())
+        tmp = &_map[curr_node->x+1][curr_node->y];
+        is_contain = std::find(open_list.begin(), open_list.end(), tmp);
+        if (tmp->attr != ATTR_FORBID && tmp->attr != ATTR_CLOSE && is_contain == open_list.end())
         {
-            set_node_variable(curr_node, next_node, _end);
-            open_list.push_back(next_node);
+            tmp->h = (abs(tmp->x - _end->x) + abs(tmp->y - _end->y))*STEP_LENGTH;
+            tmp->g = curr_node->g + STEP_LENGTH;
+            tmp->parent = curr_node;
+            open_list.push_back(tmp);
         }
     }
 
     //左
     if (curr_node->y > 0)
     {
-        next_node = &_map[curr_node->x][curr_node->y-1];
-        is_contain = std::find(open_list.begin(), open_list.end(), next_node);
-        if (next_node->attr != ATTR_FORBID && next_node->attr != ATTR_CLOSE && is_contain == open_list.end())
+        tmp = &_map[curr_node->x][curr_node->y-1];
+        is_contain = std::find(open_list.begin(), open_list.end(), tmp);
+        if (tmp->attr != ATTR_FORBID && tmp->attr != ATTR_CLOSE && is_contain == open_list.end())
         {
-            set_node_variable(curr_node, next_node, _end);
-            open_list.push_back(next_node);
+            tmp->h = (abs(tmp->x - _end->x) + abs(tmp->y - _end->y))*STEP_LENGTH;
+            tmp->g = curr_node->g + STEP_LENGTH;
+            tmp->parent = curr_node;
+            open_list.push_back(tmp);
         }
     }
 
     //右
     if (curr_node->y < MAP_COLUMN - 1)
     {
-        next_node = &_map[curr_node->x][curr_node->y+1];
-        is_contain = std::find(open_list.begin(), open_list.end(), next_node);
-        if (next_node->attr != ATTR_FORBID && next_node->attr != ATTR_CLOSE && is_contain == open_list.end())
+        tmp = &_map[curr_node->x][curr_node->y+1];
+        is_contain = std::find(open_list.begin(), open_list.end(), tmp);
+        if (tmp->attr != ATTR_FORBID && tmp->attr != ATTR_CLOSE && is_contain == open_list.end())
         {
-            set_node_variable(curr_node, next_node, _end);
-            open_list.push_back(next_node);
+            tmp->h = (abs(tmp->x - _end->x) + abs(tmp->y - _end->y))*STEP_LENGTH;
+            tmp->g = curr_node->g + STEP_LENGTH;
+            tmp->parent = curr_node;
+            open_list.push_back(tmp);
         }
     }
     return 0;
@@ -105,7 +105,7 @@ void find_destination(a_node *_start, a_node *_end, a_node _map[MAP_ROW][MAP_COL
         //printf("x=%d,y=%d\n", curr_node->x, curr_node->y);
         if (curr_node->x == _end->x && curr_node->y == _end->y)
         {
-            printf("find best way!\n");
+            printf("from %d,%d to %d,%d, find best way!\n",_start->x,_start->y,_end->x,_end->y);
             goto End;
         }
 
@@ -135,7 +135,7 @@ void print_path(a_node *node)
 
 void print_map(a_node _map[MAP_ROW][MAP_COLUMN])
 {
-    printf("print map\n");
+    printf("\n");
     for (int i = 0; i < MAP_ROW; i++)
     {
         for (int j = 0; j < MAP_COLUMN; j++)
@@ -145,7 +145,7 @@ void print_map(a_node _map[MAP_ROW][MAP_COLUMN])
             else if (_map[i][j].attr == ATTR_FORBID)
                 printf("0 ");
             else
-                printf("1 ");
+                printf("  ");
         }
         printf("\n");
     }
